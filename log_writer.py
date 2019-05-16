a4ebaabc7a3092feb34c0fd940ca62e078914fb2
MichalPiwo <48207241+MichalPiwo@users.noreply.github.com>
2019-05-17 00:49:03 +0200

    should work (#24)
    work probably

--- log_writer.py
+++ log_writer.py
@@ -16,7 +16,7 @@ class LogWriter(object):
 		#1
 		# return every second element (counting from index 1) from passed list
 		# e.g. get_every_second_element([1,2,3,4]) == [2,4]
-		pass
+		return data[1::2]
 
 	@staticmethod
 	def avg_every_second_element(data):
@@ -55,8 +55,8 @@ class LogWriter(object):
 		str = ""
 		str = self.head_text + "_________" + "\n After change: \n"
 		str += self.insert_data_in_text(self.head_text, self.list_data)
-		o_count = self.count_o(str)
-		return (str,o_count)
+		self.o_count = self.count_o(str)
+		return (str,self.o_count)
 
 	@staticmethod
 	def what_is_added_the_meaning_of_life(add=None):
@@ -74,7 +74,10 @@ class LogWriter(object):
 		#8
 		# if the argument is not specified return "To seek the holy grail"
 		# in other case append the texts "To seek the " with argument and return
-		pass
+		if quest == "holy grail":
+			return "To seek the holy grail"
+		else:
+			return "To seek the " + quest
 
 	@staticmethod
 	def get_second_word(text):
@@ -85,7 +88,9 @@ class LogWriter(object):
 		return second_word
 
 	def o_count_is_even(self):
-		if o_count % 2 == 0:
+		if self.o_count  == None:
+			return False
+		if self.o_count % 2 == 0:
 		    return True
 		else:
 		    return False
@@ -102,15 +107,15 @@ class LogWriter(object):
 		#Lastly if o_count is higher than seven append empty line and
 		#empty call of what_is_your_quest to the output.
 		#Return the output
-                result = "" 
-		if o_count_is_even():
-			result = what_is_added_the_meaning_of_life(self.o_count)
+		result = ""
+		if self.o_count_is_even():
+			result = str(self.what_is_added_the_meaning_of_life(self.o_count))
 		else:
-			result = what_is_your_quest(get_second_word(self.head_text))
-
+			result = self.what_is_your_quest(self.get_second_word(self.head_text))
+		if self.o_count  == None:
+			return result
 		if self.o_count > 7:
-			result = "\n" + what_is_your_quest()
-
+			result += "\n" + self.what_is_your_quest()
 		return result
 		pass
 
@@ -134,7 +139,11 @@ class LogWriter(object):
 		# - the value of function computation (in argument)
 		# applied on number 47
 		# to the output of get_movie_reference
-		pass
+		if computation != None:
+			Ret = self.get_movie_reference() + '\n'+ str(computation(47))
+		else:
+			Ret = self.get_movie_reference()
+		return Ret
 
 	def combining_method(self):
 		#14
@@ -143,7 +152,7 @@ class LogWriter(object):
 		# - string "0 O 0 O 0 O 0 O 0 O 0 O"
 		# - output of get_second_part applied on computation method (class member)
 		#return the concatenation
-		return self.get_first_part() + "0 O 0 O 0 O 0 O 0 O 0 O" + self.get_second_part(self.computation)
+		return self.get_first_part()[0] + "0 O 0 O 0 O 0 O 0 O 0 O" + self.get_second_part(self.computation)
 
 	def __str__(self):
 		return self.combining_method()
